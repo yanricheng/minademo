33dc5f5a8c8c1c01a8918af8615a712035f89b82
yanrc <yanrc@servyou.com.cn>
2016-10-07 15:27:21 +0800

    add time client

---
+++ src/main/java/net/yanrc/demo/time/ClientSessionHandler.java
@@ -0,0 +1,45 @@
+package net.yanrc.demo.time;
+
+import org.apache.mina.core.service.IoHandler;
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IoSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * {@link IoHandler} for SumUp client.
+ *
+ * @author <a href="http://mina.apache.org">Apache MINA Project</a>
+ */
+public class ClientSessionHandler extends IoHandlerAdapter {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ClientSessionHandler.class);
+    CountDownLatch count = null;
+
+    public ClientSessionHandler(final CountDownLatch count){
+        this.count = count;
+    }
+
+    @Override
+    public void sessionOpened(IoSession session) {
+        session.write("begin");
+    }
+
+    @Override
+    public void messageReceived(IoSession session, Object message) {
+        count.countDown();
+        if(count.getCount()==0){
+            session.write("quit");
+        }else{
+            System.out.println("client receiv :"+count.getCount()+":"+message.toString());
+        }
+    }
+
+    @Override
+    public void exceptionCaught(IoSession session, Throwable cause) {
+        session.closeNow();
+    }
+}
---
+++ src/main/java/net/yanrc/demo/time/TimeClient.java
@@ -0,0 +1,48 @@
+package net.yanrc.demo.time;
+
+import org.apache.mina.core.RuntimeIoException;
+import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.serialization.ObjectSerializationCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketConnector;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by Administrator on 2016/10/7.
+ */
+public class TimeClient {
+    private static final CountDownLatch count = new CountDownLatch(99);
+    private static long CONNECT_TIMEOUT = 1000;
+    private static String HOSTNAME = "localhost";
+    private static int PORT = 9123;
+
+    public static void main(String[] args) throws Throwable {
+        NioSocketConnector connector = new NioSocketConnector();
+        connector.setConnectTimeoutMillis(CONNECT_TIMEOUT);
+        connector.getFilterChain().addLast("codec",
+                new ProtocolCodecFilter(new ObjectSerializationCodecFactory()));
+        connector.getFilterChain().addLast("logger", new LoggingFilter());
+        connector.setHandler(new ClientSessionHandler(count));
+        IoSession session;
+
+        for (; ; )
+            try {
+                ConnectFuture future = connector.connect(new InetSocketAddress(HOSTNAME, PORT));
+                future.awaitUninterruptibly();
+                session = future.getSession();
+                break;
+            } catch (RuntimeIoException e) {
+                System.err.println("Failed to connect. ");
+                e.printStackTrace();
+                Thread.sleep(5000);
+            }
+
+        // wait until the summation is done
+        session.getCloseFuture().awaitUninterruptibly();
+        connector.dispose();
+    }
+}
