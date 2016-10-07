a55900a95107fa8afef8a787b31286443d75c6de
yanrc <yanrc@servyou.com.cn>
2016-10-07 12:04:46 +0800

    add timserver

---
+++ src/main/java/net/yanrc/demo/time/TimeServerHandler.java
@@ -0,0 +1,34 @@
+package net.yanrc.demo.time;
+
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2016/10/7.
+ */
+public class TimeServerHandler extends IoHandlerAdapter {
+    @Override
+    public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
+        cause.printStackTrace();
+    }
+
+    @Override
+    public void messageReceived(IoSession session, Object message) throws Exception {
+        String str = message.toString();
+        if (str.trim().equalsIgnoreCase("quit")) {
+            session.close();
+            return;
+        }
+        Date date = new Date();
+        session.write(date.toLocaleString());
+        System.out.println("Message written...");
+    }
+
+    @Override
+    public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
+        System.out.println("IDLE " + session.getIdleCount(status));
+    }
+}
